89bb5203decc898b959d486f7f9ad32a51c1a96e
Sam Bennrubi <sbennrubi@gmail.com>
2019-01-13 02:47:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,16 @@
 require_relative "./user.rb"
 class Student < User
+  
   def initialize
     @knowledge = []
+  end
+  
+  def learn(material)
+    @knowledge << material
   end
   
   def knowledge
     @knowledge
   end
+  
 end
